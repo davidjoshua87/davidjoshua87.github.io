19bd671b824b36af51ffda17a478b87089c953c7
David Joshua <david.joshua@mapits.com>
2021-03-24 22:14:11 +0700

    update website

---
+++ precache-manifest.c9af7a9c32d4131855d86f7cd9193cc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a27a4163254fc8fce870c8cc3a3f94f",
+    "url": "/404.html"
+  },
+  {
+    "revision": "51135704c8e45c31426c",
+    "url": "/css/app.84013b65.css"
+  },
+  {
+    "revision": "e111e240295b25e9050e",
+    "url": "/css/chunk-vendors.5058bb9d.css"
+  },
+  {
+    "revision": "25d45ce9bf63f75f9f2c22e332009daa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51135704c8e45c31426c",
+    "url": "/js/app.db9f4fcd.js"
+  },
+  {
+    "revision": "e111e240295b25e9050e",
+    "url": "/js/chunk-vendors.c0d01287.js"
+  },
+  {
+    "revision": "8eff00fb23e5046e8587fb699e79e093",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
